732c4e77b5d39865ef3197c3615f54fa9926cbf0
Olga <gorskih.olka@yandex.ru>
2019-05-07 15:51:41 +0300

    Add changes

--- solutions/src/main/java/ru/sberbank/school/task02/Client.java
+++ solutions/src/main/java/ru/sberbank/school/task02/Client.java
@@ -1,11 +1,22 @@
 package ru.sberbank.school.task02;
 
+import ru.sberbank.school.task02.util.Beneficiary;
 import ru.sberbank.school.task02.util.FxRequest;
 import ru.sberbank.school.task02.util.FxResponse;
 
 import java.util.List;
+import java.util.Map;
 
 public class Client implements FxClientController {
+
+    public static void main(String[] args) {
+        String benef = System.getenv().get("SBRF_BENEFICIARY");
+        Beneficiary beneficiary = Beneficiary.valueOf(benef.toUpperCase());
+
+        System.out.println(beneficiary);
+
+    }
+
     @Override
     public List<FxResponse> fetchResult(List<FxRequest> requests) {
         return null;
